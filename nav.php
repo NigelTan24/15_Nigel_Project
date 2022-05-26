a76f5c174fbf2bbd477294ca555d24391d6919d9
NigelTan24 <nigel_tan_wei_teng@connect.ite.edu.sg>
2022-05-26 09:15:24 +0800

    First Update
    Update 7 initial files

---
+++ nav.php
@@ -0,0 +1,9 @@
+<div id="banner" align="center"><img src="Banner.jpg" 
+width="1400" height="200" "banner"/></div>
+<link href="header.css" rel="stylesheet" type="text/css">
+<div class="topnav">
+	<a href="logcall.php">Log Call</a>
+	<a href="update.php">Update</a>
+	<a href="#">Report </a>
+	<a href="#">History</a>
+</div>
